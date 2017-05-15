4fd2c043262afdaaed05972cda3fa42d04cc4e87
Douglas Drinka <ddrinka@gmail.com>
2017-05-15 09:15:34 -0500

    Added workaround for S3Client creation. Disabled logging in the S3Client. Added additional test cases for ListObjects and GetObject.

--- src/Log4NetBug/S3Client.cs
+++ src/Log4NetBug/S3Client.cs
@@ -1,13 +1,51 @@
-﻿using Amazon.S3;
+﻿using Amazon;
+using Amazon.S3;
+using Amazon.S3.Model;
 using System;
+using System.IO;
+using System.Threading.Tasks;
 
 namespace Log4NetBug
 {
     public static class S3Client
     {
-		public static void CreateS3Client(string accessKeyId, string secretAccessKey)
+		public static AmazonS3Client CreateS3Client(string accessKeyId, string secretAccessKey)
 		{
-			var s3Client = new AmazonS3Client(accessKeyId, secretAccessKey);
+			var config = new AmazonS3Config
+			{
+				DisableLogging = true,
+				RegionEndpoint = RegionEndpoint.USEast1
+			};
+			return new AmazonS3Client(accessKeyId, secretAccessKey, config);
+		}
+
+		public async static Task ListBucketObjects(AmazonS3Client client, string bucketName)
+		{
+			var request = new ListObjectsRequest
+			{
+				BucketName = bucketName
+			};
+
+			await client.ListObjectsAsync(request)
+				.ConfigureAwait(continueOnCapturedContext: false);
+		}
+
+		public async static Task<string> GetObject(AmazonS3Client client, string bucketName, string objectName)
+		{
+			var request = new GetObjectRequest
+			{
+				BucketName = bucketName,
+				Key = objectName
+			};
+
+			using (var response = await client.GetObjectAsync(request)
+				.ConfigureAwait(continueOnCapturedContext: false))
+			{
+				using (var streamReader = new StreamReader(response.ResponseStream))
+				{
+					return streamReader.ReadToEnd();
+				}
+			}
 		}
 	}
 }
